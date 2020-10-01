bb3c96964d571a35e1afcce2bd43d01bb3224511
Александр <trigun1000@mail.ru>
2020-10-01 18:50:56 +0300

    задание 1

--- homework3.1/src/homework1/Apple.java
+++ homework3.1/src/homework1/Apple.java
@@ -1,7 +1,6 @@
 package homework1;
 
 public class Apple implements Fruit {
-
     private final double appleWeight = 1;
     @Override
     public double getWeight() {
--- homework3.1/src/homework1/Fruit.java
+++ homework3.1/src/homework1/Fruit.java
@@ -1,4 +1,6 @@
 package homework1;
+
+
 public interface Fruit {
     public double  getWeight();
 }
